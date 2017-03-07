0eaf957f3a558cd83fee50d02171a49e2708eb70
Casper Rasmussen <cr@nodes.dk>
2017-03-07 09:58:16 +0100

    updated version

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@ import PackageDescription
 let package = Package(
     name: "jwt-keychain-demo",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3),
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
         .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 1, minor: 1),
-        .Package(url: "https://github.com/nodes-vapor/jwt-keychain.git", majorVersion: 0)
+        .Package(url: "https://github.com/nodes-vapor/jwt-keychain.git", majorVersion: 0, minor: 9)
     ],
     exclude: [
         "Config",
